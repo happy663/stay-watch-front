0e7df306577f1e430e54deabadf7609253258459
happy663 <tatu2425@gmail.com>
2022-05-22 18:04:47 +0900

    serverSideProps remove

--- pages/roomHistory/index.tsx
+++ pages/roomHistory/index.tsx
@@ -8,25 +8,24 @@ import Layout from "components/common/Layout";
 import RoomHistory from "components/roomHistory/RoomHistory";
 import { baseURL } from "utils/api";
 
-type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
-export const getServerSideProps: GetServerSideProps = async () => {
-  const API_URL = `${baseURL}/room/v1/log`;
-  const res = await fetch(API_URL);
-  const data = await res.json();
+// type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const API_URL = `${baseURL}/room/v1/log`;
+//   const res = await fetch(API_URL);
+//   const data = await res.json();
 
-  console.log("SSR");
+//   console.log("SSR");
 
-  return {
-    props: {
-      fallback: {
-        [API_URL]: data,
-      },
-    },
-  };
-};
+//   return {
+//     props: {
+//       fallback: {
+//         [API_URL]: data,
+//       },
+//     },
+//   };
+// };
 
-const RoomHistoryIndex: NextPage<Props> = (props) => {
-  const { fallback } = props;
+const RoomHistoryIndex = () => {
   return (
     <SWRConfig>
       <Layout>
